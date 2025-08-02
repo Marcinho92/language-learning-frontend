25cdba374958f88b114686d034a2f407069094d8
Marcinho <marcin1897@gmail.com>
2025-08-02 22:46:46 +0200

    Optimize build process for memory efficiency - reduce Node memory usage, optimize Vite config, update Dockerfile

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-// WAŻNE: Nasłuchuj na 0.0.0.0, nie localhost!
+// KRYTYCZNE: Nasłuchuj na 0.0.0.0, nie localhost!
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
